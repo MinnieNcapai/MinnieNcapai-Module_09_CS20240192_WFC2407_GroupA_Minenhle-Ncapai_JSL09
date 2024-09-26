703aa88ca3605f08f7d43678d08efe2a70c40e61
Minenhle Ncapai <minipai64@gmail.com>
2024-09-26 14:32:56 +0200

    Add default background image and author name on error and  Added main and top div styling

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
 
+   .catch(err => { 
+        //Add default background image 
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+         // Set fallback author name
+        document.getElementById("author").textContent = `By: Dodi Achmad`
+      }) 
+  
+      fetch("https://api.coingecko.com/api/v3/coins/dogecoin")  
+      .then(res => {
+        //Check if response is ok, throw error if not
+          if (!res.ok) {
+              throw Error("Something went wrong")
+          }
+          return res.json() // Parse/convert response to JSON
+      })
+
