5a1761d52d13265747e94b8080ebd8a5181debb6
Minenhle Ncapai <minipai64@gmail.com>
2024-09-30 14:31:52 +0200

    Convert Fahrenheit to Celsius, add seconds to time display, and change USD to ZAR

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch nature image, set as background, use default on error
 try{ 
     const res = await fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
 const data = await res.json()
@@ -30,12 +31,15 @@ const data = await res.json()
         document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
+   
         `
-// Update crypto section with current, high, and low prices
+     // Assuming an exchange rate of 1 USD = 18 ZAR
+     const usdToZar = 18;
+// Update crypto section with current, high, and low prices in Zar
         document.getElementById("crypto").innerHTML += ` 
-        <p>🎯: $${data.market_data.current_price.usd}</p>
-        <p>👆: $${data.market_data.high_24h.usd}</p>
-        <p>👇: $${data.market_data.low_24h.usd}</p>
+        <p>🎯:     R${data.market_data.current_price.usd * usdToZar}</p>
+        <p>👆: R${data.market_data.high_24h.usd *usdToZar}</p>
+        <p>👇: R${data.market_data.low_24h.usd *usdToZar}</p>
     `
     }
     catch(err) {console.error(err)} // Log any errors
@@ -44,15 +48,16 @@ const data = await res.json()
         // Get current date and time
         const date = new Date()
          // Update time element with formatted time
-        document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+        document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "medium"})
     }
     
     // Update the time every second
     setInterval(getCurrentTime, 1000)
 
+    // Fetch weather data by location, handle error
     navigator.geolocation.getCurrentPosition(async position => {
         try {
-            const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+            const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`) // 'metric' for Celsius
             if (!res.ok) {
                 throw Error("Weather data not available")
             }
@@ -60,7 +65,7 @@ const data = await res.json()
             const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
             document.getElementById("weather").innerHTML = `
                 <img src=${iconUrl} />
-                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-temp">${Math.round(data.main.temp)}ºC</p>
                 <p class="weather-city">${data.name}</p>
             `
         } catch (err) {
